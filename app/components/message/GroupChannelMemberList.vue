268495887ed88672d16ee67d1f9dac87d2e2029c
willaCS <arthur.willems.prive@gmail.com>
2023-10-19 14:08:09 +0200

    changed group member list to be fully hidden and button to show them is now more discrete

--- app/components/message/GroupChannelMemberList.vue
+++ app/components/message/GroupChannelMemberList.vue
@@ -5,32 +5,37 @@ const { members } = useChannelStore()
 </script>
 
 <template>
-	<div class="h-full" :class="(isMembersOpen ? 'w-60' : '')">
-		<div class="flex p-2 bg-color1">
-			<template v-if="isMembersOpen">
+	<template v-if="isMembersOpen">
+		<div class="grid h-full grid-rows-[max-content,auto]" :class="(isMembersOpen ? 'w-60' : 'w-14')">
+			<div class="flex p-2 bg-color1">
 				<div class="flex self-center mr-auto text-3xl place-items-center">Members</div>
-			</template>
-			<GenericButton :buttonStyle="1" class="self-center w-12 h-12 mr-0"
-				@click="() => { isMembersOpen = !isMembersOpen }"
-			>
-				<Icon name="material-symbols:menu" class="w-full h-full"/>
-			</GenericButton>
-		</div>
-
-		<div class="h-full overflow-x-hidden bg-color2 scrollbar scrollbar-w-2 scrollbar-h-2 scrollbar-thumb-text scrollbar-thumb-rounded-none scrollbar-track">
-			<template v-for="member in members">
-				<div class="flex p-2">
-					<GenericProfilePicture class="w-10 h-10" imageSrc="/amogus.png"/>
-					<template v-if="isMembersOpen">
-						<div class="flex">
+				<GenericButton :buttonStyle="1" class="self-center w-12 h-12 mr-0"
+					@click="() => { isMembersOpen = !isMembersOpen }"
+				>
+					<Icon name="material-symbols:menu" class="w-full h-full"/>
+				</GenericButton>
+			</div>
+	
+			<div class="overflow-x-hidden bg-color2 scrollbar scrollbar-w-2 scrollbar-h-2 scrollbar-thumb-text scrollbar-thumb-rounded-none scrollbar-track">
+				<template v-for="member in members">
+					<GenericButton class="w-full p-1" :buttonStyle="2">
+						<GenericProfilePicture class="w-10 h-10" imageSrc="/amogus.png"/>
+						<div class="self-center pl-2">
 							{{ member.name }}
 						</div>
-					</template>
-				</div>
-			</template>
+						<div class="mr-auto"></div>
+					</GenericButton>
+				</template>
+			</div>
 		</div>
-
-	</div>
+	</template>
+	<template v-else>
+		<GenericButton :buttonStyle="1" class="w-12 h-12 mt-2 mr-4"
+			@click="() => { isMembersOpen = !isMembersOpen }"
+		>
+			<Icon name="material-symbols:menu" class="w-full h-full"/>
+		</GenericButton>
+	</template>
 </template>
 
 
