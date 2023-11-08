28d0eafd42359d7023de8f5210e4ab19116e319f
willaCS <arthur.willems.prive@gmail.com>
2023-11-08 12:21:05 +0100

    ++

--- app/components/generic/buttonStyle.ts
+++ app/components/generic/buttonStyle.ts
@@ -2,9 +2,9 @@ export function getStyle(style: number) {
 	switch (style){
 		case 1:
 			return {
-				normal: "rounded border-2 disabled:bg-background3 disabled:border-background2 hover:bg-accent-color hover:bg-opacity-60 hover:border-accent-color",
-				unselected: "border-color1 bg-color1",
-				selected: "bg-accent-color bg-opacity-15 border-accent-color",
+				normal: "rounded border-2 ",
+				unselected: "border-color1 bg-color1 hover:bg-accent-color hover:bg-opacity-60 hover:border-accent-color disabled:bg-background3 disabled:border-background2",
+				selected: "bg-accent-color border-accent-color disabled:bg-accent-color disabled:border-accent-color",
 			};
 		case 2:
 			return {
--- app/components/play/custom.vue
+++ app/components/play/custom.vue
@@ -2,6 +2,8 @@
 
 const { optionsList } = useGameStore()
 
+const isHost = ref(false)
+
 </script>
 
 <template>
@@ -10,46 +12,45 @@ const { optionsList } = useGameStore()
 			<div class="w-full p-2 text-3xl text-center bg-color3">Settings</div>
 			<div class="grid grid-cols-[auto,5rem,3rem,3rem,3rem] p-1 gap-1 w-full">
 				<div class="flex items-center h-full col-span-2">Number of Players</div>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:looks-one" class="w-full h-full"/></GenericButton>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:looks-two" class="w-full h-full"/></GenericButton>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:looks-4" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="!isHost" class="w-full h-12"><Icon name="material-symbols:looks-one" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="!isHost" class="w-full h-12"><Icon name="material-symbols:looks-two" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="!isHost" class="w-full h-12"><Icon name="material-symbols:looks-4" class="w-full h-full"/></GenericButton>
 
 				<div class="flex items-center h-full col-span-2">Max Round</div>
 				<div class="flex items-center h-full">{{ optionsList.maxPoint }}</div>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:add" class="w-full h-full"/></GenericButton>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:remove" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="!isHost" class="w-full h-12"><Icon name="material-symbols:add" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="!isHost" class="w-full h-12"><Icon name="material-symbols:remove" class="w-full h-full"/></GenericButton>
 
 				<div class="flex items-center h-full col-span-2">Ball Size</div>
 				<div class="flex items-center h-full"><span v-if="!optionsList.randomizer">{{ optionsList.ballSize / 10}}x</span></div>
-				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer" class="w-full h-12"><Icon name="material-symbols:add" class="w-full h-full"/></GenericButton>
-				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer" class="w-full h-12"><Icon name="material-symbols:remove" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer || !isHost" class="w-full h-12"><Icon name="material-symbols:add" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer || !isHost" class="w-full h-12"><Icon name="material-symbols:remove" class="w-full h-full"/></GenericButton>
 
 				<div class="flex items-center h-full col-span-2">Pad Size</div>
 				<div class="flex items-center h-full"><span v-if="!optionsList.randomizer">{{ optionsList.padSize / 100 }}x</span></div>
-				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer" class="w-full h-12">
-					<Icon name="material-symbols:add" class="w-full h-full"/>
-				</GenericButton>
-				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer" class="w-full h-12">
-					<Icon name="material-symbols:remove" class="w-full h-full"/>
-				</GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer || !isHost" class="w-full h-12"><Icon name="material-symbols:add" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" :disabled="optionsList.randomizer || !isHost" class="w-full h-12"><Icon name="material-symbols:remove" class="w-full h-full"/></GenericButton>
 
 				<div class="flex items-center h-full col-span-4">Activate Randomizer</div>
-				<GenericButton :buttonStyle="1" class="w-full h-12" @click="() => optionsList.randomizer = !optionsList.randomizer">
+				<GenericButton :buttonStyle="1" :selected="optionsList.randomizer" :disabled="!isHost" class="w-full h-12" @click="() => optionsList.randomizer = !optionsList.randomizer">
 					<Icon v-if="optionsList.randomizer" name="material-symbols:check-box" class="w-full h-full"/>
 					<Icon v-else name="material-symbols:check-box-outline-blank" class="w-full h-full"/>
 				</GenericButton>
 				
 				<div class="flex items-center h-full col-span-2">Bot Difficulty</div>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:sentiment-neutral" class="w-full h-full"/></GenericButton>
-				<GenericButton :buttonStyle="1" class="w-full h-12"><Icon name="material-symbols:sentiment-dissatisfied" class="w-full h-full"/></GenericButton>
-				<GenericButton :buttonStyle="1" class="w-full h-12" :selected="true"><Icon name="material-symbols:sentiment-extremely-dissatisfied" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" class="w-full h-12" :disabled="!isHost" :selected="optionsList.mode === 'easy'"  @click="() => optionsList.mode = 'easy'"> <Icon name="material-symbols:sentiment-neutral" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" class="w-full h-12" :disabled="!isHost" :selected="optionsList.mode === 'hard'"  @click="() => optionsList.mode = 'hard'"> <Icon name="material-symbols:sentiment-dissatisfied" class="w-full h-full"/></GenericButton>
+				<GenericButton :buttonStyle="1" class="w-full h-12" :disabled="!isHost" :selected="optionsList.mode === 'crazy'" @click="() => optionsList.mode = 'crazy'"><Icon name="material-symbols:sentiment-extremely-dissatisfied" class="w-full h-full"/></GenericButton>
 			</div>
-			<client-only placeholder="loading...">
+			{{ isHost }}
+			<!-- <client-only placeholder="loading...">
 				<GamePreviewCanvas/>
-			</client-only>
+			</client-only> -->
 
 			<div class="w-full p-2">
-				<GenericButton :button-style="1" class="flex w-full p-2">
+				<GenericButton :button-style="1" class="flex w-full p-2"
+					@click="() => { isHost = !isHost; console.log(isHost) }"
+				>
 					Play
 				</GenericButton>
 			</div>
