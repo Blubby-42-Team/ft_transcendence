0080161cd26883aaab6760effb0ffdef2cf974d4
willaCS <arthur.willems.prive@gmail.com>
2023-11-21 13:39:21 +0100

    Fix front-end rendering issues

--- app/components/play/custom.vue
+++ app/components/play/custom.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 
-
-
-let isFront = useState(() => [1, 0, 0, 0]);
+let isFront = useState(() => [
+	{ type: CardType.PLAYER },
+	{ type: CardType.ADD },
+	{ type: CardType.ADD },
+	{ type: CardType.EMPTY },
+]);
 
 </script>
 
@@ -10,28 +13,9 @@ let isFront = useState(() => [1, 0, 0, 0]);
 	<div class="grid h-full grid-cols-[max-content_auto]">
 		<PlaySettings/>
 		<div class="grid h-full grid-cols-[repeat(4,1fr)] w-full">
-			<template v-for="i in 4">
-				<div class="p-4">
-					<div class="relative w-full h-full">
-						<transition name="flip" mode="in-out">
-							<div v-if="isFront[i - 1] === 1" class="absolute w-full h-full align-top card">
-								<div class="w-full h-full border-4 bg-gradient-to-bl from-yellow-400 via-accent-color to-yellow-400 rounded-2xl border-color2">
-									<div class="h-full w-full p-2 bg-gradient-to-tl grid from-yellow-600 via-transparent to-yellow-600 grid-rows-[9rem_max-content_repeat(4,auto)] rounded-xl justify-items-center">
-										<GenericProfilePicture imageSrc="/amogus.png" class="w-32 h-32"/>
-										<div class="flex w-full pt-2 overflow-hidden text-2xl font-bold truncate text-background2">James Milwaukee</div>
-									</div>
-								</div>
-							</div>
-							<div v-else-if="isFront[i - 1] === 0" class="absolute w-full h-full align-top card">
-								<GenericButton
-									class="w-full h-full border-4 bg-background3 hover:bg-color2 rounded-2xl border-color2"
-									@click="() => isFront[i - 1] = 1"
-								>
-									<Icon name="material-symbols:add" class="w-full h-full text-color3"/>
-								</GenericButton>
-							</div>
-						</transition>
-					</div>
+			<template v-for="el of isFront">
+				<div class="p-2">
+					<PlayLobbyCard :type="el.type" @click="() => el.type = CardType.PLAYER"/>
 				</div>
 			</template>
 		</div>
