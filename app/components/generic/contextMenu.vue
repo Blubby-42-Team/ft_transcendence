88d04e8378e1d0aea3597a3d02b41261719362d1
willaCS <arthur.willems.prive@gmail.com>
2023-11-23 21:18:12 +0100

    Fix context menu position overflow

--- app/components/generic/contextMenu.vue
+++ app/components/generic/contextMenu.vue
@@ -13,25 +13,25 @@ const position = ref({x: 0, y: 0})
 
 function close(){ isOpen.value = false; }
 function open(event: MouseEvent) {
-	console.log('open');
-	console.log(event)
 	isOpen.value = true;
 		
 	nextTick(() => {
-		// Calculate initial position based on mouse click
 		if (!modal.value)
 			return;
 
 		position.value.x = event.clientX;
 		position.value.y = event.clientY;
-	
-		// Adjust if the modal overflows the right side of the screen
-		if (position.value.x + modal.value.clientWidth > window.innerWidth) {
-			position.value.x = window.innerWidth - modal.value.clientWidth;
-			console
+
+		if (position.value.y - modal.value.clientHeight < 0
+			&& position.value.y + modal.value.clientHeight > window.innerHeight
+		) {
+			position.value.y = position.value.y - modal.value.clientHeight / 2;
 		}
 	
-		// Adjust if the modal overflows the bottom of the screen
+		if (position.value.x + modal.value.clientWidth > window.innerWidth) {
+			position.value.x = window.innerWidth - modal.value.clientWidth;
+		}
+	
 		if (position.value.y > window.innerHeight - modal.value.clientHeight){
 			position.value.y = position.value.y - modal.value.clientHeight;
 		}
