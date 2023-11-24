28d9d2a46e97a98c4559c8c5eed0fb98fe917e3a
willaCS <arthur.willems.prive@gmail.com>
2023-11-24 14:04:10 +0100

    wip sideMenu

--- app/components/generic/sideMenu.vue
+++ app/components/generic/sideMenu.vue
@@ -5,14 +5,11 @@ const props = defineProps({
 		type: String as PropType<'left' | 'right' | 'top' | 'bottom' | 'vertical' | 'horizontal'>,
 		required: true,
 	},
-	reference: {
-		type: Object as PropType<HTMLElement | undefined>,
-		required: true,
-	}
 })
 
 const isOpen = ref(false);
-const menu = ref<HTMLInputElement>()
+const menuRef	= ref<HTMLInputElement>()
+const buttonRef	= ref<HTMLInputElement>()
 const position = ref({
 	x: 0,
 	y: 0,
@@ -20,10 +17,11 @@ const position = ref({
 })
 
 function updateSize(){
-	if (!menu.value || !props.reference)
+	console.log('updateSize')
+	if (!menuRef.value || !buttonRef.value)
 		return;
 	
-	const { top, left, width, height } = props.reference.getBoundingClientRect();
+	const { top, left, width, height } = buttonRef.value.getBoundingClientRect();
 
 	position.value.x		= left;
 	position.value.y		= top + height;
@@ -36,12 +34,19 @@ function open() {
 	nextTick(updateSize);
 }
 
+// This watch doesnt work. It doesnt update the size of the menu when the button is clicked.
+watchEffect(() => {
+  if (buttonRef.value) {
+    updateSize();
+  }
+});
+
 defineExpose({
 	open,
 	close,
 })
 
-onClickOutside(menu, () => {
+onClickOutside(menuRef, () => {
 	if (isOpen.value) {
 		close()
 	}
@@ -50,21 +55,23 @@ onClickOutside(menu, () => {
 </script>
 
 <template>
-	<TransitionExpand
-		
-	>
-		<template v-if="isOpen">
-			<div class="absolute z-50 bg-red-700 w-max h-max" ref="menu">
-				<div class="h-max"
-					:style="{
-						width: `${position.width}px`,
-						left: `${position.x}px`,
-						top:  `${position.y}px`,
-					}"
-				>
-					<slot/>
+	<div ref="buttonRef">
+		<slot name="buttontest" />
+		<TransitionExpand>
+			<template v-if="isOpen">
+				<div class="absolute z-50 bg-red-700 w-max h-max" ref="menuRef">
+					<div class="h-max"
+						:style="{
+							width: `${position.width}px`,
+							left: `${position.x}px`,
+							top:  `${position.y}px`,
+						}"
+					>
+						<slot name="contenttest"/>
+					</div>
 				</div>
-			</div>
-		</template>
-	</TransitionExpand>
+			</template>
+		</TransitionExpand>
+
+	</div>
 </template>
--- app/pages/friends.vue
+++ app/pages/friends.vue
@@ -5,7 +5,6 @@ const { setPageData } = usePageStore();
 onMounted(() => { setPageData(EPageCategories.FRIENDS, "My Friends"); })
 
 const testRef		= ref()
-const testButton	= ref<HTMLElement>()
 
 const test: Array<{ text: string, icon: string } | undefined> = [
 	{ text: 'Zoom',				icon: 'material-symbols:zoom-in' },
@@ -21,31 +20,32 @@ const test: Array<{ text: string, icon: string } | undefined> = [
 
 <template>
 	<div class="h-full p-5">
-		<div :buttonStyle="1" class="p-5 bg-red-900 hover:bg-red-950 rounded-2xl" @click="testRef?.open" ref="testButton">Hello World</div>
-		<GenericSideMenu ref="testRef"
-			:reference="testButton"
-			direction="bottom"
-		>
-			<div class="w-full px-5 pt-1">
-				<div class="flex flex-col w-full px-2 py-4 text-sm border-4 rounded-lg shadow-lg border-color3 text-text bg-color2">
-					<!-- <GenericProfilePicture imageSrc="/amogus.png"/> -->
-					<div class="w-full p-2 mt-3 text-base">James Milwaukee</div>
-					<hr class="my-3 border-color3" />
-					<template v-for="elem in test">
-						<template v-if="elem">
-							<button class="flex px-2 py-1 rounded cursor-pointer hover:bg-accent-color hover:text-color2">
-								<div class="w-5">
-									<Icon :name="elem.icon" class="w-4 h-4"/>
-								</div>
-								<div class="ml-4">{{ elem.text }}</div>
-							</button>
+		<GenericSideMenu ref="testRef" direction="bottom">
+			<template #buttontest>
+				<div :buttonStyle="1" class="p-5 bg-red-900 hover:bg-red-950 rounded-2xl" @click="testRef?.open" ref="testButton">Hello World</div>
+			</template>
+			<template #contenttest>
+				<div class="w-full px-5 pt-1">
+					<div class="flex flex-col w-full px-2 py-4 text-sm border-4 rounded-lg shadow-lg border-color3 text-text bg-color2">
+						<!-- <GenericProfilePicture imageSrc="/amogus.png"/> -->
+						<div class="w-full p-2 mt-3 text-base">James Milwaukee</div>
+						<hr class="my-3 border-color3" />
+						<template v-for="elem in test">
+							<template v-if="elem">
+								<button class="flex px-2 py-1 rounded cursor-pointer hover:bg-accent-color hover:text-color2">
+									<div class="w-5">
+										<Icon :name="elem.icon" class="w-4 h-4"/>
+									</div>
+									<div class="ml-4">{{ elem.text }}</div>
+								</button>
+							</template>
+							<template v-else>
+								<hr class="my-3 border-color3" />
+							</template>
 						</template>
-						<template v-else>
-							<hr class="my-3 border-color3" />
-						</template>
-					</template>
+					</div>
 				</div>
-			</div>
+			</template>
 		</GenericSideMenu>
 	</div>
 </template>
