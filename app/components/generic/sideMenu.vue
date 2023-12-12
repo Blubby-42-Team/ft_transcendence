55803e7a2c905f253d5a0c36f45ae8aad79b2e92
willaCS <arthur.willems.prive@gmail.com>
2023-12-12 18:49:58 +0000

    simplification

--- app/components/generic/sideMenu.vue
+++ app/components/generic/sideMenu.vue
@@ -11,66 +11,78 @@ const props = defineProps({
 		type: String as PropType<'left' | 'right' | 'top' | 'bottom' | 'middle'>,
 		default: 'middle',
 	},
+	containerRef: {
+		type: Function as PropType<() => Ref<HTMLElement | undefined>>,
+		required: true,
+	},
 })
 
-
 const isOpen		= ref(false);
-const contentRef	= ref<HTMLInputElement>()
-const buttonRef		= ref<HTMLInputElement>()
 const position		= ref({
 	x: 0,
 	y: 0,
 })
 
-function open() { isOpen.value = true; startUpdateLoop()  }
+let startLoop = () => {};
+const menuRef	= ref<HTMLInputElement>();
+
+onMounted(() => {
+	const buttonRef = props?.containerRef();
+	
+	startLoop = () => {
+		nextTick(async () => {
+			while (isOpen.value && menuRef.value && buttonRef.value) {
+				const button = buttonRef.value.getBoundingClientRect();
+				const menu = menuRef.value.getBoundingClientRect();
+				
+				switch (props.direction) {
+					case 'vertical':
+					case 'bottom':
+					case 'top':
+						switch (props.alignDirection) {
+							case 'right':	position.value.x = (button.left < window.innerWidth - menu.width ? button.left : window.innerWidth - menu.width); break;
+							case 'left':	position.value.x = (button.left + button.width - menu.width > 0 ? position.value.x = button.left + button.width - menu.width : 0); break;
+							case 'middle':	position.value.x = button.left + button.width / 2 - menu.width / 2; break;
+							default: break;
+						}
+						break;
+					case 'horizontal':
+					case 'right':
+					case 'left':
+						switch (props.alignDirection) {
+							case 'bottom':	position.value.y = (button.top < window.innerHeight - menu.height ? button.top : window.innerHeight - menu.height); break;
+							case 'top':		position.value.y = (button.top + button.height - menu.height > 0 ? position.value.x = button.top + button.height - menu.height : 0); break;
+							case 'middle':	position.value.y = button.top + button.height / 2 - menu.height / 2; break;
+							default: break;
+						}
+					default:
+						break;
+				}
+
+				switch (props.direction) {
+					case 'vertical':	position.value.y = (2 * button.top + button.height < window.innerHeight ? button.top + button.height : button.top - menu.height); break;
+					case 'bottom':		position.value.y = button.top + button.height; break;
+					case 'top':			position.value.y = button.top - menu.height; break;
+					case 'horizontal':	position.value.x = (2 * button.left + button.width < window.innerWidth ? button.left + button.width : button.left - menu.width); break;
+					case 'right':		position.value.x = button.left + button.width; break;
+					case 'left':		position.value.x = button.left - menu.width; break;
+					default: break;
+				}
+				await new Promise(resolve => setTimeout(resolve, 10));
+			}
+		})
+	}
+})
+
+function open() { isOpen.value = true; startLoop()  }
 function close(){ isOpen.value = false; }
 
-function startUpdateLoop(){
-	nextTick(async () => {
-		while (isOpen.value && contentRef.value && buttonRef.value) {
-			const button = buttonRef.value.getBoundingClientRect();
-			const content = contentRef.value.getBoundingClientRect();
+defineExpose({
+	open,
+	close,
+})
 
-			switch (props.direction) {
-				case 'vertical':
-				case 'bottom':
-				case 'top':
-					switch (props.alignDirection) {
-						case 'right':	position.value.x = (button.left < window.innerWidth - content.width ? button.left : window.innerWidth - content.width); break;
-						case 'left':	position.value.x = (button.left + button.width - content.width > 0 ? position.value.x = button.left + button.width - content.width : 0); break;
-						case 'middle':	position.value.x = button.left + button.width / 2 - content.width / 2; break;
-						default: break;
-					}
-					break;
-				case 'horizontal':
-				case 'right':
-				case 'left':
-					switch (props.alignDirection) {
-						case 'bottom':	position.value.y = (button.top < window.innerHeight - content.height ? button.top : window.innerHeight - content.height); break;
-						case 'top':		position.value.y = (button.top + button.height - content.height > 0 ? position.value.x = button.top + button.height - content.height : 0); break;
-						case 'middle':	position.value.y = button.top + button.height / 2 - content.height / 2; break;
-						default: break;
-					}
-				default:
-					break;
-			}
-
-			switch (props.direction) {
-				case 'vertical':	position.value.y = (2 * button.top + button.height < window.innerHeight ? button.top + button.height : button.top - content.height); break;
-				case 'bottom':		position.value.y = button.top + button.height; break;
-				case 'top':			position.value.y = button.top - content.height; break;
-				case 'horizontal':	position.value.x = (2 * button.left + button.width < window.innerWidth ? button.left + button.width : button.left - content.width); break;
-				case 'right':		position.value.x = button.left + button.width; break;
-				case 'left':		position.value.x = button.left - content.width; break;
-				default: break;
-			}
-
-			await new Promise(resolve => setTimeout(resolve, 10));
-		}
-	})
-}
-
-onClickOutside(contentRef, () => {
+onClickOutside(menuRef, () => {
 	if (isOpen.value) {
 		close()
 	}
@@ -79,19 +91,16 @@ onClickOutside(contentRef, () => {
 </script>
 	
 <template>
-	<div ref="buttonRef" @click="open" class="flex w-min h-min">
-		<slot name="reference"/>
-	</div>
-	<!-- <TransitionExpand> -->
+	<TransitionExpand>
 		<template v-if="isOpen">
-			<div class="absolute h-max w-max" ref="contentRef"
+			<div class="absolute h-max w-max" ref="menuRef"
 				:style="{
 					left: `${position.x}px`,
 					top:  `${position.y}px`,
 				}"
 			>
-				<slot name="thisContent"/>
+				<slot/>
 			</div>
 		</template>
-	<!-- </TransitionExpand> -->
+	</TransitionExpand>
 </template>
--- app/pages/friends.vue
+++ app/pages/friends.vue
@@ -8,6 +8,7 @@ const testRef		= ref()
 const testButton	= ref<HTMLElement>()
 
 const test: Array<{ text: string, icon: string } | undefined> = [
+	undefined,
 	{ text: 'Zoom',				icon: 'material-symbols:zoom-in' },
 	{ text: 'Zoom Out',			icon: 'material-symbols:zoom-out' },
 	{ text: 'Full Screen',		icon: 'material-symbols:open-in-full' },
@@ -17,42 +18,40 @@ const test: Array<{ text: string, icon: string } | undefined> = [
 	{ text: 'Add to Wallet',	icon: 'material-symbols:wallet' },
 ]
 
+const accessRef = () => testButton
+
 </script>
 
 <template>
-	<div class="flex flex-row-reverse h-full p-5">
+	<div class="h-full p-5">
 		<div class="flex bg-blue-500 w-[22rem]">Hello</div>
-		<GenericSideMenu ref="testRef" direction="left" alignDirection="bottom">
-			<template #reference>
-				<GenericButton :buttonStyle="1"
-					class="w-32 p-2"
-					ref="testButton"
-				>
-					Hello World
-				</GenericButton>
-			</template>
-			<template #thisContent>
-				<div class="w-full">
-					<div class="flex flex-col w-full px-2 py-4 text-sm border-4 rounded-lg shadow-lg border-color1 bg-background1">
-						<GenericProfilePicture imageSrc="/amogus.png" class="w-96 h-96"/>
-						<div class="w-full p-2 mt-3 text-base">James Milwaukee</div>
-						<hr class="my-3 border-text" />
-						<template v-for="elem in test">
-							<template v-if="elem">
-								<button class="flex px-2 py-1 rounded cursor-pointer hover:bg-accent1 hover:text-text-dark">
-									<div class="w-5">
-										<Icon :name="elem.icon" class="w-4 h-4"/>
-									</div>
-									<div class="ml-4">{{ elem.text }}</div>
-								</button>
-							</template>
-							<template v-else>
-								<hr class="my-3 border-text" />
-							</template>
+		<div ref="testButton" class="bg-red-500 w-max h-max">
+			<GenericButton :buttonStyle="1" @click="testRef?.open"
+				class="w-32 h-10 p-2"
+			>
+				Hello World
+			</GenericButton>
+		</div>
+		<GenericSideMenu ref="testRef" direction="bottom" alignDirection="left" :containerRef="accessRef">
+			<div class="w-full">
+				<div class="flex flex-col w-full px-2 py-4 text-sm border-4 rounded-lg shadow-lg border-color1 bg-background1">
+					<GenericProfilePicture imageSrc="/amogus.png" class="w-96 h-96"/>
+					<div class="w-full p-2 mt-3 text-base">James Milwaukee</div>
+					<template v-for="elem in test">
+						<template v-if="elem">
+							<button class="flex px-2 py-1 rounded cursor-pointer hover:bg-accent1 hover:text-text-dark">
+								<div class="w-5">
+									<Icon :name="elem.icon" class="w-4 h-4"/>
+								</div>
+								<div class="ml-4">{{ elem.text }}</div>
+							</button>
 						</template>
-					</div>
+						<template v-else>
+							<hr class="my-3 border-text" />
+						</template>
+					</template>
 				</div>
-			</template>
+			</div>
 		</GenericSideMenu>
 	</div>
 </template>
