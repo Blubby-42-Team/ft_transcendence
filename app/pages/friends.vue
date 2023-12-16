e2ca814e416d36cbf7a1a9a23b5d8950fb63aed6
willaCS <arthur.willems.prive@gmail.com>
2023-12-16 18:27:29 +0000

    added friends page

--- app/pages/friends.vue
+++ app/pages/friends.vue
@@ -4,23 +4,12 @@ definePageMeta({name: 'Friends'})
 const { setSelectedCategory } = usePageStore();
 onMounted(() => { setSelectedCategory(EPageCategories.FRIENDS); })
 
-const testRef		= ref();
-const testButton	= ref();
-
-const test = () => testButton;
-
 </script>
 
 <template>
-	<div class="h-full p-5">
-		<div class="flex bg-blue-500 w-[22rem]">Hello</div>
-		<div ref="testButton">
-			<GenericButton :buttonStyle="1" @click="testRef?.open" class="w-32 h-10 p-2" >
-				Hello World
-			</GenericButton>
-		</div>
-		<GenericSideMenu ref="testRef" direction="bottom" alignDirection="right" :accessRef="test">
-			<MessageProfile/>
-		</GenericSideMenu>
+	<div class="grid h-full grid-rows-[4rem,1fr] grid-cols-[max-content,auto]">
+		<div class="col-span-2 bg-color1"></div>
+		<FriendsList class="scrollbar scrollbar-w-2 scrollbar-h-2 scrollbar-thumb-color1 scrollbar-thumb-rounded-full scrollbar-track"/>
+		<Profile class="max-h-full overflow-hidden" :userId="1" />
 	</div>
 </template>
