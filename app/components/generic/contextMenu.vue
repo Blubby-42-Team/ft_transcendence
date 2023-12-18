c4a860904c7a1921da41556d7bbee66174b75901
willaCS <arthur.willems.prive@gmail.com>
2023-12-18 14:26:10 +0000

    wip interactions

--- app/components/generic/contextMenu.vue
+++ app/components/generic/contextMenu.vue
@@ -1,39 +1,47 @@
 <script setup lang="ts">
 
 const isOpen = ref(false);
+const lastMouseEvent = ref();
 const menu = ref<HTMLInputElement>()
 const position = ref({x: 0, y: 0})
 
 function close(){ isOpen.value = false; }
 function open(event: MouseEvent) {
 	isOpen.value = true;
-		
-	nextTick(() => {
-		if (!menu.value || !event)
+	lastMouseEvent.value = event;
+}
+
+function updateMenu(){
+	if (!menu.value || !lastMouseEvent.value)
 			return;
 
-		position.value.x = event.clientX;
-		position.value.y = event.clientY;
+	position.value.x = lastMouseEvent.value.clientX;
+	position.value.y = lastMouseEvent.value.clientY;
 
-		if (position.value.y - menu.value.clientHeight < 0
-			&& position.value.y + menu.value.clientHeight > window.innerHeight
-		) {
-			position.value.y = position.value.y - menu.value.clientHeight / 2;
-		}
-	
-		if (position.value.x + menu.value.clientWidth > window.innerWidth) {
-			position.value.x = window.innerWidth - menu.value.clientWidth;
-		}
-	
-		if (position.value.y > window.innerHeight - menu.value.clientHeight){
-			position.value.y = position.value.y - menu.value.clientHeight;
-		}
-	})
+	if (position.value.y - menu.value.clientHeight < 0
+		&& position.value.y + menu.value.clientHeight > window.innerHeight
+	) {
+		position.value.y = position.value.y - menu.value.clientHeight / 2;
+	}
+
+	if (position.value.x + menu.value.clientWidth > window.innerWidth) {
+		position.value.x = window.innerWidth - menu.value.clientWidth;
+	}
+
+	if (position.value.y > window.innerHeight - menu.value.clientHeight){
+		position.value.y = position.value.y - menu.value.clientHeight;
+	}
 }
 
 defineExpose({
 	open,
 	close,
+})
+
+onMounted(() => {
+	watch([isOpen, lastMouseEvent], () => {
+		nextTick(updateMenu);
+	})
 })
 
 onClickOutside(menu, () => {
@@ -49,7 +57,7 @@ onClickOutside(menu, () => {
 		<teleport to='#modals'>
 			<TransitionFade>
 				<template v-if="isOpen">
-					<div ref="menu" class="absolute w-max h-max"
+					<div ref="menu" class="absolute overflow-hidden w-max h-max"
 						:style="{
 							left: `${position.x}px`,
 							top:  `${position.y}px`,
