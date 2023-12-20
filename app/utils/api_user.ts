b5dc162453c4dd64dc566a5ddb2b822e98dcb054
Basile <basile_deleeuw@yahoo.be>
2023-12-20 13:19:57 +0000

    api_user done

--- app/utils/api_user.ts
+++ app/utils/api_user.ts
@@ -3,10 +3,151 @@ export default {
 		userId: number,
 		callback: (response: any) => void,
 	){
-		return useFetch(`${process.env.NUXT_BACK_URI}/game/room`, {
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/${userId}`, {
 			onResponse: ({ request, response, options }) => {
 				callback(response);
 				console.log('user fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		})
+	},
+
+	fetchUserFriends(
+		userId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/friends/${userId}`, {
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user friends fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		})
+	},
+
+	fetchUserWhitelist(
+		userId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/whitelist/${userId}`, {
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user whitelist fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		})
+	},
+
+	fetchUserBlacklist(
+		userId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/blacklist/${userId}`, {
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user blacklist fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		})
+	},
+
+	fetchUserIsInBlacklist(
+		userId: number,
+		blacklistId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/blacklist/is_in/${userId}/${blacklistId}`, {
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user is in blacklist fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		})
+	},
+
+	fetchUserWhitelistPost(
+		userId: number,
+		whitelistId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/whitelist/${userId}`, {
+			method: 'POST',
+			body: {
+				'id': whitelistId
+			},
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user whitelist fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		  })
+	},
+
+	fetchUserBlacklistPost(
+		userId: number,
+		blacklistId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/blacklist/${userId}`, {
+			method: 'POST',
+			body: {
+				'id': blacklistId
+			},
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user blacklist fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		  })
+	},
+
+	fetchUserFriendDelete(
+		userId: number,
+		friendId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/friends/${userId}`, {
+			method: 'DELETE',
+			body: {
+				'id': friendId
+			},
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user delete friend fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		  })
+	},
+
+	fetchUserBlacklistDelete(
+		userId: number,
+		blacklistId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/user/blacklists/${userId}`, {
+			method: 'DELETE',
+			body: {
+				'id': blacklistId
+			},
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user delete blacklist fetched');
 			},
 			onRequestError: ({ request, error, options }) => {
 				console.warn('error', error);
