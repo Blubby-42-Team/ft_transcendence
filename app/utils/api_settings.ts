d8564542b51a5a6dc6b25e64f108b0aebda0a48d
Basile <basile_deleeuw@yahoo.be>
2023-12-20 14:22:23 +0000

    api_stats & api_settings added

---
+++ app/utils/api_settings.ts
@@ -0,0 +1,43 @@
+export enum ETheme {
+	Light = 'light',
+	Dark = 'dark'
+}
+
+export default {
+	fetchSettings(
+		userId: number,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/settings/${userId}`, {
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('settings fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		})
+	},
+
+    fetchSettingsPatch(
+		userId: number,
+		theme: ETheme,
+        sound: boolean,
+		callback: (response: any) => void,
+	){
+		return useFetch(`${process.env.NUXT_BACK_URI}/settings/${userId}`, {
+			method: 'PATCH',
+			body: {
+				'theme': theme,
+                'sound': sound,
+			},
+			onResponse: ({ request, response, options }) => {
+				callback(response);
+				console.log('user game save fetched');
+			},
+			onRequestError: ({ request, error, options }) => {
+				console.warn('error', error);
+			},
+		  })
+	},
+}
