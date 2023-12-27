b1a0d349761219634967145a947e1634f04b0024
willaCS <arthur.willems.prive@gmail.com>
2023-12-27 14:14:33 +0000

    fix error 500 when refresing after cookie

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,4 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+	if (process.server){
+		return ;
+	}
 	const config = useRuntimeConfig();
 	const cookie = useCookie('user_id', {
 		default: () => 0,
