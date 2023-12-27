87f3ed4e8b8050986f2f215bb6c2580c05879bef
willaCS <arthur.willems.prive@gmail.com>
2023-12-27 14:36:31 +0000

    update profile to be dynamic

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -6,8 +6,25 @@ onMounted(() => { setSelectedCategory(EPageCategories.NONE); })
 
 const { primaryUser } = useUserStore();
 
+const openFindFriend = ref();
+
 </script>
 
 <template>
-	<Profile :userId="primaryUser.id"/>
+	<div class="h-full">
+		<ClientOnly>
+			<teleport to="#additionalHeaderButton">
+				<div class="mx-2 border border-text-light bg-text-light"></div>
+				<GenericButton :buttonStyle="1" class="self-center w-12 h-12"
+					@click="openFindFriend?.open"
+				>
+					<Icon name="material-symbols:edit" class="w-full h-full"/>
+				</GenericButton>
+				<GenericModal ref="openFindFriend">
+					<ProfileRename :close="openFindFriend?.close"/>
+				</GenericModal>
+			</teleport>
+		</ClientOnly>
+		<Profile :userId="primaryUser.id"/>
+	</div>
 </template>
