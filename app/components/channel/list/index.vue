d1990326482003fde173cefa3c228453e20a8bf4
willaCS <arthur.willems.prive@gmail.com>
2023-12-29 11:17:11 +0000

    update channel list to only view groups that already have channels in it

--- app/components/channel/list/index.vue
+++ app/components/channel/list/index.vue
@@ -6,10 +6,28 @@ const { selectedChannel, channels } = useChannelStore()
 
 const hasSideMenu = useState<boolean>('hasSideMenu', () => false);
 const types = useState(() => [
-	{ type: [EChatType.friends],                     name: 'Friends', open: true, hasBottom: true,  hasSideMenu: false, icon: 'material-symbols:person' },
-	{ type: [EChatType.group],                       name: 'Groups',  open: true, hasBottom: true,  hasSideMenu: true,  icon: 'material-symbols:diversity-4' },
-	{ type: [EChatType.public, EChatType.protected], name: 'Chats',   open: true, hasBottom: false, hasSideMenu: true,  icon: 'material-symbols:groups' },
+	{ type: [EChatType.friends],                     name: 'Friends', open: true,  hasBottom: true,  hasSideMenu: false, channels: [] as Array<IShortChannel>, icon: 'material-symbols:person' },
+	{ type: [EChatType.group],                       name: 'Groups',  open: true,  hasBottom: true,  hasSideMenu: true,  channels: [] as Array<IShortChannel>, icon: 'material-symbols:diversity-4' },
+	{ type: [EChatType.public, EChatType.protected], name: 'Chats',   open: true,  hasBottom: false, hasSideMenu: true,  channels: [] as Array<IShortChannel>, icon: 'material-symbols:groups' },
 ]);
+
+const last = computed(() => {
+	for (const type of types.value.reverse()){
+		if (type.channels.length > 0){
+			return type.name;
+		}
+	}
+})
+
+types.value.forEach((elem) => {
+	elem.channels = channels.value.filter((channel) => channel !== undefined && elem.type.includes(channel.type));
+});
+
+watch(channels, () => {
+	types.value.forEach((elem) => {
+		elem.channels = channels.value.filter((channel) => channel !== undefined && elem.type.includes(channel.type));
+	});
+});
 
 watch(selectedChannel, () => {
 	if (!selectedChannel.value){
@@ -23,39 +41,41 @@ watch(selectedChannel, () => {
 
 <template>
 	<div class="w-full h-full overflow-x-hidden scrollbar scrollbar-w-0 bg-background1">
-		<template v-for="ctype in types">
-			<GenericButton class="w-full h-12 p-2 text-lg place-content-start" :buttonStyle="2"
-				:selected="selectedChannel !== null && ctype.type.includes(selectedChannel.type)"
-				@click="ctype.open = !ctype.open"
-			>
-				<Icon :name="ctype.icon" class="w-10 h-10"/>
-				<div class="pl-2">{{ ctype.name }}</div>
-			</GenericButton>
-
-			<div class="border-b-[1px] border-background2"></div>
-
-			<TransitionExpand>
-				<template v-if="ctype.open">
-					<div>
-						<template v-for="channel in channels.filter((elem) => elem !== undefined && ctype.type.includes(elem?.type))">
-							<GenericNuxtLink class="w-full px-3 py-1 place-content-start"
-								:buttonStyle="2"
-								:selected="channel?.id === selectedChannel?.id"
-								:to="`/messages/${channel?.id ?? 0}`"
-							>
-								<GenericProfilePicture class="w-10 h-10" :imageSrc="channel?.avatar ?? '/pp.png'"/>
-								<div class="pl-2">{{ channel?.name }}</div>
-							</GenericNuxtLink>
-						</template>
-					</div>
-				</template>
-			</TransitionExpand>
-
-			<TransitionExpand>
-				<template v-if="ctype.hasBottom && ctype.open">
-					<div class="pb-4 border-b-[1px] border-background2"></div>
-				</template>
-			</TransitionExpand>
+		<template v-for="(ctype, i) in types">
+			<template v-if="ctype.channels.length > 0">
+				<GenericButton class="w-full h-12 p-2 text-lg place-content-start" :buttonStyle="2"
+					:selected="selectedChannel !== null && ctype.type.includes(selectedChannel.type)"
+					@click="ctype.open = !ctype.open"
+				>
+					<Icon :name="ctype.icon" class="w-10 h-10"/>
+					<div class="pl-2">{{ ctype.name }}</div>
+				</GenericButton>
+	
+				<div class="border-b-[1px] border-background2"></div>
+	
+				<TransitionExpand>
+					<template v-if="ctype.open">
+						<div>
+							<template v-for="channel in channels.filter((elem) => elem !== undefined && ctype.type.includes(elem?.type))">
+								<GenericNuxtLink class="w-full px-3 py-1 place-content-start"
+									:buttonStyle="2"
+									:selected="channel?.id === selectedChannel?.id"
+									:to="`/messages/${channel?.id ?? 0}`"
+								>
+									<GenericProfilePicture class="w-10 h-10" :imageSrc="channel?.avatar ?? '/pp.png'"/>
+									<div class="pl-2">{{ channel?.name }}</div>
+								</GenericNuxtLink>
+							</template>
+						</div>
+					</template>
+				</TransitionExpand>
+	
+				<TransitionExpand>
+					<template v-if="last !== ctype.name && ctype.hasBottom && ctype.open">
+						<div class="pb-4 border-b-[1px] border-background2"></div>
+					</template>
+				</TransitionExpand>
+			</template>
 		</template>
 	</div>
 </template>
