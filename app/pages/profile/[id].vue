13c47eb8113198df2ccaa90b18ed7b155704b673
willaCS <arthur.willems.prive@gmail.com>
2024-01-02 12:37:03 +0000

    added add friend interaction

--- app/pages/profile/[id].vue
+++ app/pages/profile/[id].vue
@@ -5,7 +5,9 @@ const { setSelectedCategory } = usePageStore();
 onMounted(() => { setSelectedCategory(EPageCategories.NONE); })
 
 const { primaryUser } = useUserStore();
-const route = useRoute();
+const route = useRoute();7
+
+const openAddFriend = ref();
 
 const userId = (typeof route.params.id === 'string' && !isNaN(parseInt(route.params.id))) ? parseInt(route.params.id) : 0;
 </script>
@@ -16,10 +18,15 @@ const userId = (typeof route.params.id === 'string' && !isNaN(parseInt(route.par
 			<template v-if="userId !== primaryUser.id">
 				<Teleport to="#additionalHeaderButton">
 					<div class="mx-2 border border-text-light bg-text-light"></div>
-					<GenericButton :buttonStyle="1" class="self-center w-12 h-12">
+					<GenericButton :buttonStyle="1" class="self-center w-12 h-12"
+						@click="openAddFriend?.open"
+					>
 						<Icon name="material-symbols:add" class="w-full h-full"/>
 					</GenericButton>
 				</Teleport>
+				<GenericModal ref="openAddFriend">
+					<FriendsInteractionAdd :closeFunc="openAddFriend?.close" :userId="userId"/>
+				</GenericModal>
 			</template>
 		</ClientOnly>
 		<Profile :userId="userId"/>
